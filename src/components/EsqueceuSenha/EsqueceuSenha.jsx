6e9de85da7084da68c67ceadacfcc91d279bf14f
Diego Bicelli <72448015+DiegoBicelli@users.noreply.github.com>
2024-11-18 23:42:45 -0300

    [FIX](CODE) - organização e remoção de codigo inutil

--- src/components/EsqueceuSenha/EsqueceuSenha.jsx
+++ src/components/EsqueceuSenha/EsqueceuSenha.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
-import { getAuth, sendPasswordResetEmail } from 'firebase/auth'; // Importações necessárias do Firebase
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'; 
 import './EsqueceuSenha.css';
 
 const EsqueceuSenha = () => {
   const [email, setEmail] = useState('');
-  const [message, setMessage] = useState(''); // Estado para exibir mensagens de erro/sucesso
-  const [emailSent, setEmailSent] = useState(false); // Novo estado para controlar se o e-mail foi enviado
+  const [message, setMessage] = useState(''); 
+  const [emailSent, setEmailSent] = useState(false); 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const auth = getAuth();
 
     try {
-      await sendPasswordResetEmail(auth, email); // Envia o e-mail de redefinição
-      setEmailSent(true); // Define que o e-mail foi enviado com sucesso
+      await sendPasswordResetEmail(auth, email); // função pra enviar o email
+      setEmailSent(true); 
       setMessage('E-mail de recuperação enviado! Verifique sua caixa de entrada.');
     } catch (error) {
       console.error('Erro ao enviar e-mail de recuperação: ', error);
-      // Define mensagens de erro com base no código do erro
+   
       switch (error.code) {
         case 'auth/user-not-found':
           setMessage('Usuário não encontrado. Verifique o e-mail digitado.');
@@ -36,7 +36,7 @@ const EsqueceuSenha = () => {
       <div className="card-EsqueceuSenha">
         <h2 className="title-EsqueceuSenha">Esqueci a Senha</h2>
 
-        {!emailSent ? ( // Exibe o formulário enquanto o e-mail não for enviado
+        {!emailSent ? ( 
           <form onSubmit={handleSubmit}>
             <input
               type="email"
@@ -50,7 +50,7 @@ const EsqueceuSenha = () => {
               Enviar Código
             </button>
           </form>
-        ) : ( // Exibe uma mensagem de confirmação após o envio
+        ) : ( 
           <p className="success-message">Um e-mail de recuperação foi enviado para: <strong>{email}</strong>. Verifique sua caixa de entrada.</p>
         )}
 
